20f6285b1651d125ac22560e7ae748a96f23a1c4
longmenegib <gibrillongmene@gmail.com>
2022-03-14 20:06:58 +0100

    deleted package-lock

--- src/layout/SlideLayout.js
+++ src/layout/SlideLayout.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Container, Row, Col, Button} from 'reactstrap'
-import illus from '../assets/illustration.png'
+import illus from '../assets/slide.jpg'
 
 export default function SlideLayout() {
   return (
